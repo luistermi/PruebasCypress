00dfd0a16d74c84350628b27401c52f48005a229
luistermi <luis.termi.i@gmail.com>
2022-08-08 17:25:58 -0300

    Bef/aft all each

--- cypress/integration/pruebas2.js
+++ cypress/integration/pruebas2.js
@@ -1,9 +1,22 @@
 describe("Pruebas",function(){
-    it("Prueba1", function(){
+    before(()=>{
+        cy.log("Inicio general");
+    });
+    after(()=>{
+        cy.log("Fin general");
+    });
+    beforeEach(()=>{
         cy.visit("http://automationpractice.com/index.php");
+    });
+    it("Prueba1", function(){
         cy.get('#search_query_top').type("shoes");
         cy.get('#searchbox > .btn').click();
         cy.get('.lighter').contains("shoes");
         
-    })
+    });
+    it("Prueba2", function(){
+        cy.get('#search_query_top').type("dress");
+        cy.get('#searchbox > .btn').click();
+        cy.get('.lighter').contains("dress");
+    });
 })
