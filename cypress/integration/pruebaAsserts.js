07de578633037d46dc94d8194f3fcf3da14551d6
luistermi <luis.termi.i@gmail.com>
2022-08-09 12:14:29 -0300

    Prueba con asserts

---
+++ cypress/integration/pruebaAsserts.js
@@ -0,0 +1,32 @@
+describe("Prueba distintos asserts", function(){
+
+    let a = 2;
+    it("Prueba1", function(){
+        let a = 1, b = 1;
+        expect(a===b).to.equal(true);
+    });
+    it("Prueba2", function(){
+        expect(20-20).to.not.equal(1);
+    });
+
+    it("Prueba3", function(){
+        expect(20===20).to.be.true;
+    });
+    
+    it("Prueba4", function(){
+        expect(20===21).to.be.false;
+    });
+
+    it("Prueba5", function(){
+        expect(20).to.be.lessThan(21);
+    });
+
+    it("Prueba6", function(){
+        expect(20).to.be.greaterThan(19);
+    });
+
+    it("Prueba7", function(){
+        expect(a).to.exist;
+    });
+
+})
